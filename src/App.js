c0e506af33056e50c09cac387ed27c8dbf8ca791
saoud-ahmed-khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-11-09 16:30:54 +0500

    axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
-
+import { React , useState} from "react";
+// import axios from 'axios';
 function App() {
+  const[data,setData]= useState([]);
+  // axios.get("https://jsonplaceholder.typicode.com/posts")
+  // .then((res)=>{setData(res)})
+  // .catch((e)=>console.log(e))
+  // console.log(data.title)
   return (
     <div className="App">
       

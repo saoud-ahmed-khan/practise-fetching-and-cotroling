03fdbc093c37300b10f244f877640047006cb533
saoud-ahmed-khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-11-12 22:39:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import './App.css';
-import { React , useState,useEffect} from "react";
-// import axios from 'axios';
+import { React, useState, useEffect } from "react";
+import axios from 'axios';
 function App() {
-  const[data,setData]= useState(0);
+  const [data, setData] = useState(0);
+useEffect(()=>{
 
-  // axios.get("https://jsonplaceholder.typicode.com/posts")
-  // .then((res)=>{setData(res)})
-  // .catch((e)=>console.log(e))
-  // console.log(data.title)
-    
-    useEffect(() => {
-      document.title = `You clicked ${data} times`;
-      console.log(data);
-    },[data]);
+  axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => { setData(res) })
+        .catch((e) => console.log(e))
+      console.log(data.title)
+
+},[])
+
   return (
     <div className="App">
-      <h1>{data}</h1>
-      <button onClick={()=>{setData(data+1)}} >click</button>
+      
     </div>
   );
 }
